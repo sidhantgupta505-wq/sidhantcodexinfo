787e18ff039c22a08ca2a98bb6a36f4a65098cdb
sidhantgupta505-wq <sidhantgupta505@gmail.com>
2026-07-30 07:46:32 -0700

    Update sidhantkumargupta.py

--- sidhantkumargupta.py
+++ sidhantkumargupta.py
@@ -1191,10 +1191,7 @@ function displayResults(number, data) {
         recordCount.textContent = totalRecords;
         
         let html = '';
-        //const info = results[0];
-        results.forEach((info,index) =>{
-        });
-                
+        const info = results[0]        
         
         html += `<div class="result-item">
             <span class="label"><i class="fas fa-phone"></i> Phone</span>

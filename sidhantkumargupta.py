493d20f287876931ca734fd4150e904527227b71
sidhantgupta505-wq <sidhantgupta505@gmail.com>
2026-07-31 13:37:18 +0530

    Update sidhantkumargupta.py

--- sidhantkumargupta.py
+++ sidhantkumargupta.py
@@ -1332,8 +1332,9 @@ async function searchNumber() {
         const data = await callAPI(number);
         displayResults(number, data);
         
-        if (data.status === 'success' && data.result && data.result.length > 0) {
-            statusText.textContent = '✅ Search completed (' + data.result.length + ' records found)';
+       // if (data.status === 'success' && data.result && data.result.length > 0) {
+            //statusText.textContent = '✅ Search completed (' + data.result.length + ' records found)';
+            statusText.textContent = '✅ Search completed (' + results.length + ' record' + (results.length > 1 ? 's' : '') + ' found)';
             statusDot.className = 'dot';
         } else if (data.status === 'error') {
             statusText.textContent = '❌ API Error';

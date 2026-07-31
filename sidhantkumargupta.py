a724fba38b45991cddda7d8f8dd6bc71f4da147d
sidhantgupta505-wq <sidhantgupta505@gmail.com>
2026-07-31 13:18:06 +0530

    Update sidhantkumargupta.py

--- sidhantkumargupta.py
+++ sidhantkumargupta.py
@@ -1195,8 +1195,8 @@ uniqueResults.forEach((info, index) => {
     history += `Status : SUCCESS ✅\n\n`;
 
     history += `🆔 Aadhaar Number   : ${info.aadhar || 'N/A'}\n`;
-    history += `👤 Full Name : ${(info.name || 'N/A').replace(/\s+/g, ' ').trim()}\n`;
-    history += `👨 Father's Name : ${(info.fname || 'N/A').replace(/\s+/g, ' ').trim()}\n`;
+    history += `👤 Full Name      : ${info.name || 'N/A'}\n`;
+    history += `👨 Father's Name  : ${info.fname || 'N/A'}\n`;
     history += `📞 Phone            : ${info.num || '+91 ' + number}\n`;
     history += `☎️ Alternate Number : ${info.alt || 'N/A'}\n`;
 }
